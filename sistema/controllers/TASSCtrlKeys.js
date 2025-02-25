d2c2b4190a7bcad527ea9eb9fd56636a00049598
David <rexx84@gmail.com>
2025-02-25 01:41:40 -0300

    Update 25022025

--- sistema/controllers/TASSCtrlKeys.js
+++ sistema/controllers/TASSCtrlKeys.js
@@ -1777,7 +1777,7 @@ keys.controller('KeysCtrl', function($scope, $compile, $location, $routeParams, 
                             $scope.addMultiKeys(obj);
                         break;
                         case "downloadKeyChainFileList":
-                            var idKeychainStatus  = $scope.select.keychainStatus.idKeychainStatus!=undefined?$scope.select.keychainStatus.idKeychainStatus:null;
+                            var idKeychainStatus  = $scope.select.keychainStatus!=undefined?$scope.select.keychainStatus.idKeychainStatus:null;
                             var filterCategoryKey = $scope.select.filterCategoryKey!=undefined?$scope.select.filterCategoryKey:null;
                             var idDepartmenKf     = $scope.select.idDepartmenKf!=undefined?$scope.select.idDepartmenKf!=undefined:null;
                             $scope.setKeyChainListAsArrayFn($scope.customerFound.idClient,null,filterCategoryKey,idKeychainStatus,idDepartmenKf,0,null, false, false);
