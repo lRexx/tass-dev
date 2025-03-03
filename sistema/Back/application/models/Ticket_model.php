f37897c5dc504446b08243ba840ca51797d1af71
David <rexx84@gmail.com>
2025-03-03 15:48:46 -0300

    Update 03032025

--- sistema/Back/application/models/Ticket_model.php
+++ sistema/Back/application/models/Ticket_model.php
@@ -2764,7 +2764,7 @@ class Ticket_model extends CI_Model
 		//var_dump($todo);
 		$quuery  = $this->db->select("*")->from("tb_statusticket")->get();
 		foreach (@$quuery->result_array() as $item => $status) {
-			//print_r(" id: ".strval($status['idStatus'])." status: ".str_replace(' ', '_', $status['statusName'])."\n");
+			print_r(" id: ".strval($status['idStatus'])." status: ".str_replace(' ', '_', $status['statusName'])."\n");
 			$this->db->select("*_")->from("tb_tickets");
 			$this->db->where("idStatusTicketKf", "1");
 			$count = $this->db->count_all_results();
