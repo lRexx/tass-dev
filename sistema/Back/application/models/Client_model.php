673cbab1d125eae8130059826d9bf1232ccf3d4f
David <rexx84@gmail.com>
2025-03-31 23:53:16 -0300

    Update 31032025

--- sistema/Back/application/models/Client_model.php
+++ sistema/Back/application/models/Client_model.php
@@ -2413,7 +2413,8 @@ class Client_model extends CI_Model {
                     $service = $this->db->get();
                     if ($service->num_rows()>0) {
                         if (!is_null($ticket['idContrato'])){
-                            print_r($service->result_array());
+                            //print_r($service->result_array());
+                            print_r($ticket[$key]);
                             $ticket[$key]=$service->result_array();
                         }
                     }
