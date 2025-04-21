c02ee739be4d6435b80c0262fc19d96e442aaef6
David <rexx84@gmail.com>
2025-04-21 00:04:36 -0300

    Update 21042025

--- sistema/controllers/TASSCtrlTickets.js
+++ sistema/controllers/TASSCtrlTickets.js
@@ -2517,11 +2517,15 @@ tickets.controller('TicketsCtrl', function($scope, $compile, $location, $interva
                             ($scope.ticket.building.isStockInBuilding!=null && $scope.ticket.building.isStockInBuilding!='0' && $scope.ticket.building.isStockInBuilding=='1') && ($scope.ticket.building.isStockInOffice==null || $scope.ticket.building.isStockInOffice=='0')||
                             ($scope.ticket.building.isStockInOffice!=null && $scope.ticket.building.isStockInOffice!='0' && $scope.ticket.building.isStockInOffice=='1') && ($scope.ticket.building.isStockInBuilding==null || $scope.ticket.building.isStockInBuilding=='0')){
                             $scope.whoPickUpList.push({'id': 2, 'fullNameUser': "Encargado", 'type':"Otros"});
+                            for (var qtty = 0; qtty<$scope.ticket.building.initial_delivery[0].initial_qtty; qtty++){
+                                $scope.mainSwitchFn('addKeyFieldsToList', $scope.select.products.selected, $scope.rsCustomerAccessControlDoors)
+                            }
                         }
                         if ($scope.ticket.building!=undefined && (($scope.ticket.building.initial_delivery.length==0) || ($scope.ticket.building.initial_delivery.length==1 && $scope.ticket.building.initial_delivery[0].expiration_state!=undefined && $scope.ticket.building.initial_delivery[0].expiration_state))){
                             $scope.whoPickUpList.push({'id': 3, 'fullNameUser': "Tercera Persona", 'type':"Otros"});
                         }
                         console.log($scope.whoPickUpList);
+                        
                     break;
                     case "selectedUser":
                         $scope.selectedUser=obj!=undefined?obj:undefined;
