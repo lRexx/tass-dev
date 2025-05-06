7194dbc64a044378d564527ca96a6f152ae3c008
David <rexx84@gmail.com>
2025-05-06 15:32:46 -0300

    Update 06052025

--- sistema/Back/application/models/Ticket_model.php
+++ sistema/Back/application/models/Ticket_model.php
@@ -2784,7 +2784,7 @@ class Ticket_model extends CI_Model
 				$where = "(isInitialDeliveryActive = '".@$data['isInitialDeliveryActive']."')";
 				$this->db->where($where);
 			}else{
-				$where = "(ISNULL(isInitialDeliveryActive) OR isInitialDeliveryActive = '".@$data['isInitialDeliveryActive']."' OR isInitialDeliveryActive <> '".@$data['isInitialDeliveryActive']."')";
+				$where = "(ISNULL(isInitialDeliveryActive) OR isInitialDeliveryActive = '0')";
 				$this->db->where($where);
 			}
 			//MP PAYMENT Succeeded
