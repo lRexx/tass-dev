d5f7bd9e3e8b36645f04915173c75e28ac339462
David <rexx84@gmail.com>
2025-05-05 23:42:03 -0300

    Update 05052025

--- sistema/Back/application/models/Ticket_model.php
+++ sistema/Back/application/models/Ticket_model.php
@@ -3166,7 +3166,7 @@ class Ticket_model extends CI_Model
 			$quuery                     = $this->db->where("idPayment = " , @$ticket['idPaymentDeliveryKf'])->get();
 			$rs_tickets['tickets'][$key]['paymentDeliveryDetails'] = @$quuery->result_array()[0];
 
-			$this->db->select("*")->from("tb_ticket_changes_history");
+			$this->db->select("*,tb_ticket_changes_history.descripcion as description,")->from("tb_ticket_changes_history");
 			$this->db->join('tb_ticket_changes_category' , 'tb_ticket_changes_category.id = tb_ticket_changes_history.idCambiosTicketKf' , 'left');
 			$this->db->join('tb_user', 'tb_user.idUser = tb_ticket_changes_history.idUserKf', 'left');
 			$this->db->join('tb_profile', 'tb_profile.idProfile = tb_user.idProfileKf', 'left');
