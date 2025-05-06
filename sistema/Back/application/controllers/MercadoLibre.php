c6732e40e7e7e3e4654a1b935696603522cb5fb4
David <rexx84@gmail.com>
2025-05-06 00:42:43 -0300

    Update 06052025

--- sistema/Back/application/controllers/MercadoLibre.php
+++ sistema/Back/application/controllers/MercadoLibre.php
@@ -18,17 +18,17 @@ class MercadoLibre extends REST_Controller
 	{
 
 		$headers = $this->input->request_headers();
-		log_message('info', 'Host				  :' . @$headers['Host']);
-		log_message('info', 'User-Agent		  :' . @$headers['User-Agent']);
-		log_message('info', 'Accept			  :' . @$headers['Accept']);
-		log_message('info', 'Content-Typ		  :' . @$headers['Content-Type']);
-		log_message('info', 'X-Forwarded-For	  :' . @$headers['X-Forwarded-For']);
-		log_message('info', 'X-Forwarded-Host	  :' . @$headers['X-Forwarded-Host']);
+		log_message('info', 'Host               :' . @$headers['Host']);
+		log_message('info', 'User-Agent         :' . @$headers['User-Agent']);
+		log_message('info', 'Accept             :' . @$headers['Accept']);
+		log_message('info', 'Content-Typ        :' . @$headers['Content-Type']);
+		log_message('info', 'X-Forwarded-For    :' . @$headers['X-Forwarded-For']);
+		log_message('info', 'X-Forwarded-Host   :' . @$headers['X-Forwarded-Host']);
 		log_message('info', 'X-Forwarded-Server :' . @$headers['X-Forwarded-Server']);
-		log_message('info', 'Content-Length	  :' . @$headers['Content-Length']);
-		log_message('info', 'Connection		  :' . @$headers['Connection']);		
-		log_message('info', 'x-signature 		  :' . @$headers['x-signature']);
-		log_message('info', 'x-request-id 	  :' . @$headers['x-request-id']);
+		log_message('info', 'Content-Length     :' . @$headers['Content-Length']);
+		log_message('info', 'Connection         :' . @$headers['Connection']);		
+		//log_message('info', 'x-signature        :' . @$headers['x-signature']);
+		//log_message('info', 'x-request-id       :' . @$headers['x-request-id']);
 		$body = file_get_contents('php://input');
 		log_message('info', 'Cuerpo de la notificación: ' . $body);
 		$rr = null;
