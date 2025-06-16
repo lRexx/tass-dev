861f5ce8303b2607eca58df6a40c397c0f56f49a
David <rexx84@gmail.com>
2025-06-16 14:22:09 -0300

    Update 16062025

--- sistema/controllers/TASSCtrlTickets.js
+++ sistema/controllers/TASSCtrlTickets.js
@@ -1779,7 +1779,7 @@ tickets.controller('TicketsCtrl', function($scope, $compile, $location, $interva
         *            SHOW ONLY ADMIN AND COMPANY          *
         *                 CUSTOMER OPTIONS                *
         **************************************************/
-            $scope.checkDeliveryMethod = function(item){                
+            $scope.checkDeliveryMethod = function(item){
                 if(($scope.ticket.building!=undefined && $scope.ticket.building.initial_delivery.length==1 && $scope.ticket.building.initial_delivery[0].expiration_state!=undefined && !$scope.ticket.building.initial_delivery[0].expiration_state) || 
                    ($scope.ticket.building!=undefined && $scope.ticket.building.isStockInBuilding=='1' && $scope.ticket.building.isStockInBuilding!=null && $scope.ticket.building.isStockInBuilding!='0' && (
                    $scope.ticket.building.isStockInOffice=='0' || $scope.ticket.building.isStockInOffice==null || $scope.ticket.building.isStockInOffice=='0')) || 
