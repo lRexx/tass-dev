6d7a997ec76e644c1bebd1f80da43a6ab7a18901
David <rexx84@gmail.com>
2025-06-17 20:56:26 -0300

    Update 17062025

--- sistema/Back/application/controllers/Ticket.php
+++ sistema/Back/application/controllers/Ticket.php
@@ -27,7 +27,20 @@ class Ticket extends REST_Controller {
     	/* SERVICIO QUE CREA UN TICKET Nuevo */
 	public function index2_post()
 	{
-
+		$headers = $this->input->request_headers();
+		log_message('info', 'Host               :' . @$headers['Host']);
+		log_message('info', 'User-Agent         :' . @$headers['User-Agent']);
+		log_message('info', 'Accept             :' . @$headers['Accept']);
+		log_message('info', 'Content-Typ        :' . @$headers['Content-Type']);
+		log_message('info', 'X-Forwarded-For    :' . @$headers['X-Forwarded-For']);
+		log_message('info', 'X-Forwarded-Host   :' . @$headers['X-Forwarded-Host']);
+		log_message('info', 'X-Forwarded-Server :' . @$headers['X-Forwarded-Server']);
+		log_message('info', 'Content-Length     :' . @$headers['Content-Length']);
+		log_message('info', 'Connection         :' . @$headers['Connection']);		
+		//log_message('info', 'x-signature        :' . @$headers['x-signature']);
+		//log_message('info', 'x-request-id       :' . @$headers['x-request-id']);
+		$body = file_get_contents('php://input');
+		log_message('info', 'Cuerpo de la notificación: ' . $body);
 		$rs = $this->ticket_model->add2($this->post('ticket'));
 		if (!is_null($rs)){
 			$this->response(array('response' => $rs) , 200);
