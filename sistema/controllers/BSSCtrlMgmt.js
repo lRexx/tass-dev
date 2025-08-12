83175d651f36648346241e5e9dbb52f5b1743ea7
David <rexx84@gmail.com>
2025-08-11 23:43:58 -0300

    Update 11082025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -2719,7 +2719,9 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
                   $scope.monitor.filter.topfilter           = $scope.filters.topDH;
                   $scope.monitor.filter.idMgmtMethodKf      = null;
                   $scope.monitor.filter.idStatusTicketKf    = "8";
-                  $scope.filters.ticketStatus               = {idStatus: '8', statusName: 'En preparacion'}
+                  $scope.filters.ticketStatus               = {idStatus: '8', statusName: 'En preparacion'};
+                  console.log($scope.monitor.filter);
+                  console.log($scope.filters);
                   $scope.listTickets($scope.monitor.filter);
                 break;
                 case "4":
