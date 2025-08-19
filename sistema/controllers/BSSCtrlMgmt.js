6430d984dee4977a74868308fc6ef6d977ef0568
David <rexx84@gmail.com>
2025-08-18 23:53:35 -0300

    Update 16082025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -436,7 +436,9 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
                   $('#confirmRequestModalCustom').modal('hide');
                   $scope.update.ticket = {};
                   $scope.update.ticket = $scope.keyObj;
-                  $scope.update.ticket.idNewStatusKf = "4";
+                  $scope.update.ticket.idNewStatusKf        = "4";
+                  $scope.update.ticket.deliveryCompany      = $scope.listDeliveryCompanies.find(s => s.idDeliveryCompany == 1);
+                  $scope.update.ticket.idDeliveryCompanyKf  = $scope.update.ticket.deliveryCompany.idDeliveryCompany;
                   $scope.modalConfirmation('setDeliveryPending',0, $scope.update);
                 }
               break;
