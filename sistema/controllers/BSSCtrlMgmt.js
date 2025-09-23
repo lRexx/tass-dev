6810c0643992001d56912a63202efde3357d35c8
David <rexx84@gmail.com>
2025-09-22 23:32:53 -0300

    Update 22092025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -3757,9 +3757,9 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
                 case "2":
                 case "6":
                   if (!$scope.tkupdate.idMgmtMethodKf){
-                    $scope.getKeychainListFn($scope.tkupdate.building.idClient,null,"6","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,1,1);
+                    $scope.getKeychainListFn($scope.tkupdate.building.idClient,null,"2","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,1,1);
                   }else{
-                    $scope.getKeychainListFn($scope.tkupdate.building.idClient,null,"2","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,0,1);
+                    $scope.getKeychainListFn($scope.tkupdate.building.idClient,null,"6","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,0,1);
                   }
 
                 break
