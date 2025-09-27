ff8770ea615b7777b372b687898591b67b775675
David <rexx84@gmail.com>
2025-09-27 16:20:33 -0300

    Update 27092025

--- sistema/controllers/TASSCtrlTickets.js
+++ sistema/controllers/TASSCtrlTickets.js
@@ -4578,9 +4578,9 @@ tickets.controller('TicketsCtrl', function($scope, $compile, $location, $interva
                             }
                         }
                         console.log($scope.new.ticket);
-                        $('#showModalRequestStatus').modal({backdrop: 'static', keyboard: false});
+                        //$('#showModalRequestStatus').modal({backdrop: 'static', keyboard: false});
                         $timeout(function() {
-                           $scope.addUpRequestFn($scope.new);
+                           //$scope.addUpRequestFn($scope.new);
                         }, 2000);
 
                     break;
