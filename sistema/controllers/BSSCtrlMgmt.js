120430c3d4dd8c2a4e3892bb755d9960a5ce6103
David <rexx84@gmail.com>
2025-09-30 01:00:10 -0300

    Update 30092025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -3692,7 +3692,7 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
                               $scope.isCodeExist=false;
                             break;
                           }
-                          if(!$scope.isCodeExist && !isCodeNewExist){
+                          if(!$scope.isCodeExist && !$scope.isCodeNewExist){
                             console.log("ADD_NO_EXIST");
                             let depto = $scope.tmpKey.new.Depto!=undefined?$scope.tmpKey.new.Depto:null;
                             let idDepto = $scope.tmpKey.new.department!=undefined?$scope.tmpKey.new.department:null;
