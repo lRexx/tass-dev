b320f9f1befaca757918ab77c666863b82a75ee8
David <rexx84@gmail.com>
2025-09-30 19:22:26 -0300

    Update 30092025

--- sistema/Back/application/models/Ticket_model.php
+++ sistema/Back/application/models/Ticket_model.php
@@ -1249,7 +1249,7 @@ class Ticket_model extends CI_Model
 					'idStatusTicketKf' => @$ticket['idNewStatusKf']
 				)
 			)->where("idTicket", $ticket['idTicket'])->update("tb_tickets_2");
-		} else if (($ticket['idTypeRequestFor'] == "1" || $ticket['idTypeRequestFor'] == "5" || $ticket['idTypeRequestFor'] == "6") && !is_null($ticket['idTypeDeliveryKf']) && $ticket['idTypeDeliveryKf'] == "2" && !is_null($ticket['delivery_schedule_at']) && is_null($ticket['delivered_at'])) {
+		} else if (($ticket['idTypeRequestFor'] == "1" || $ticket['idTypeRequestFor'] == "2" || $ticket['idTypeRequestFor'] == "5" || $ticket['idTypeRequestFor'] == "6") && !is_null($ticket['idTypeDeliveryKf']) && $ticket['idTypeDeliveryKf'] == "2" && !is_null($ticket['delivery_schedule_at']) && is_null($ticket['delivered_at'])) {
 			$this->db->set(
 				array(
 					'idStatusTicketKf' => @$ticket['idNewStatusKf'],
@@ -1257,7 +1257,7 @@ class Ticket_model extends CI_Model
 					'idDeliveryCompanyKf' => @$ticket['idDeliveryCompanyKf']
 				)
 			)->where("idTicket", $ticket['idTicket'])->update("tb_tickets_2");
-		} else if ((($ticket['idTypeRequestFor'] == "1" || $ticket['idTypeRequestFor'] == "5" || $ticket['idTypeRequestFor'] == "6") && !is_null($ticket['idTypeDeliveryKf']) && $ticket['idTypeDeliveryKf'] == "2" && !is_null($ticket['delivered_at'])) || ($ticket['idTypeRequestFor'] == "2" && !is_null($ticket['delivered_at']))) {
+		} else if ((($ticket['idTypeRequestFor'] == "1" || $ticket['idTypeRequestFor'] == "2" || $ticket['idTypeRequestFor'] == "5" || $ticket['idTypeRequestFor'] == "6") && !is_null($ticket['idTypeDeliveryKf']) && $ticket['idTypeDeliveryKf'] == "2" && !is_null($ticket['delivered_at'])) || ($ticket['idTypeRequestFor'] == "2" && !is_null($ticket['delivered_at']))) {
 			$this->db->set(
 				array(
 					'idStatusTicketKf' => @$ticket['idNewStatusKf'],
