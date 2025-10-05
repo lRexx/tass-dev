f13f838c9a6359ed3b374b177b0b9f998e146fa0
David <rexx84@gmail.com>
2025-10-05 20:53:49 -0300

    Update 05102025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -1293,7 +1293,7 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
                   default:
                     if (($scope.tkupdate.building.isStockInBuilding!='0' && $scope.tkupdate.building.isStockInBuilding!=null && $scope.tkupdate.building.isStockInBuilding!=undefined) || ($scope.tkupdate.building.isStockInOffice!='0' && $scope.tkupdate.building.isStockInOffice!=null && $scope.tkupdate.building.isStockInOffice!=undefined)){
                       console.log("Get Stock Key List");
-                      $scope.getKeychainListFnNew($scope.tkupdate.building.idClient,null,"2","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,1,1).then(function(response) {
+                      $scope.getKeychainListFnNew($scope.tkupdate.building.idClient,null,"8","-1",null,null,null,1,$scope.pagination.pageSizeSelected,false,true,1,1).then(function(response) {
                         console.log(response);
                           if(response.status==undefined){
                             $scope.existingStockKeys = response;
