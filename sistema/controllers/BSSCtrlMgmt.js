31453df4fea7033a37cd159803056d4179f98888
David <rexx84@gmail.com>
2025-10-29 23:16:35 -0300

    Update 29102025

--- sistema/controllers/BSSCtrlMgmt.js
+++ sistema/controllers/BSSCtrlMgmt.js
@@ -1173,7 +1173,7 @@ mgmt.controller('MgmtCtrl', function($scope, $rootScope, $http, $location, $rout
               if(response.status==200){
                 console.log(response.data[0]);
                 $scope.rsData.ticket = (response.data.tickets[0]);
-                $scope.tkupdate = response.data.tickets[0];
+                $scope.tkupdate = response.data.tickets;
                 $scope.getContractsByCustomerIdFn($scope.tkupdate.building.idClient);
                 $scope.getKeysAssociatedToACustomerFn($scope.tkupdate.building.idClient);
                 $scope.getControlAccessDoorsAssociatedToACustomerFn($scope.tkupdate.building.idClient);
