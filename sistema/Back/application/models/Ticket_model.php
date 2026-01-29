1fcddea7635f2951dfb1692aa78cfcc551768bd3
David <rexx84@gmail.com>
2026-01-29 20:01:01 -0300

    Update 29012026

--- sistema/Back/application/models/Ticket_model.php
+++ sistema/Back/application/models/Ticket_model.php
@@ -2615,7 +2615,7 @@ class Ticket_model extends CI_Model
 			}
 			//TECHNNICIAN ASSIGNED
 			if (@$data['isTechnicianAssigned'] == '1') {
-				$where = "(ISNULL(isTechnicianAssigned) OR isTechnicianAssigned = '" . @$data['isTechnicianAssigned'] . "')";
+				$where = "isTechnicianAssigned = '" . @$data['isTechnicianAssigned'] . "')";
 				$this->db->where($where);
 			} else if (@$data['isTechnicianAssigned'] == '0') {
 				$where = "(ISNULL(isTechnicianAssigned) OR isTechnicianAssigned = '" . @$data['isTechnicianAssigned'] . "')";
