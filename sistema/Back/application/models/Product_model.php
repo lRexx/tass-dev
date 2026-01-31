f0da8fe404951eacfe73b091773f0371b6ebeca9
David <rexx84@gmail.com>
2026-01-30 22:05:19 -0300

    Update 30012026

--- sistema/Back/application/models/Product_model.php
+++ sistema/Back/application/models/Product_model.php
@@ -1,44 +1,53 @@
-<?php if (! defined('BASEPATH'))
+<?php if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Product_model extends CI_Model {
+class Product_model extends CI_Model
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function add($product) {
+    public function add($product)
+    {
 
         $user = null;
 
         $this->db->select("*")->from("tb_products");
         $query = $this->db->where("tb_products.descriptionProduct =", $product['descriptionProduct'])->get();
         if ($query->num_rows() < 1) {
-            $this->db->insert('tb_products', [
-                    'descriptionProduct'        => $product['descriptionProduct'],
-                    'codigoFabric'              => $product['codigoFabric'],
-                    'brand'                     => $product['brand'],
-                    'model'                     => $product['model'],
+            $this->db->insert(
+                'tb_products',
+                [
+                    'descriptionProduct' => $product['descriptionProduct'],
+                    'codigoFabric' => $product['codigoFabric'],
+                    'brand' => $product['brand'],
+                    'model' => $product['model'],
                     'idProductClassificationFk' => $product['idProductClassificationFk'],
-                    'isNumberSerieFabric'       => $product['isNumberSerieFabric'],
-                    'isNumberSerieInternal'     => $product['isNumberSerieInternal'],
-                    'isDateExpiration'          => $product['isDateExpiration'],
-                    'isControlSchedule'         => $product['isControlSchedule'],
-                    'isRequestNumber'           => $product['isRequestNumber'],
-                    'priceFabric'               => $product['priceFabric'],
+                    'isNumberSerieFabric' => $product['isNumberSerieFabric'],
+                    'isNumberSerieInternal' => $product['isNumberSerieInternal'],
+                    'isDateExpiration' => $product['isDateExpiration'],
+                    'isControlSchedule' => $product['isControlSchedule'],
+                    'isRequestNumber' => $product['isRequestNumber'],
+                    'isLicenseDevice' => $product['isLicenseDevice'],
+                    'priceFabric' => $product['priceFabric'],
                 ]
             );
 
 
             if ($this->db->affected_rows() === 1) {
                 $idProductFk = $this->db->insert_id();
-                if (@$product['list_id_divice']!=null){
+                if (@$product['list_id_divice'] != null) {
                     if (count(@$product['list_id_divice']) > 0) {
                         foreach ($product['list_id_divice'] as $valor) {
 
-                            $this->db->insert('tb_products_divice_opening', [
-                                    'idProductFk'       => $idProductFk,
-                                    'idDiviceOpeningFk' => $valor['idDiviceOpeningFk'] ]
+                            $this->db->insert(
+                                'tb_products_divice_opening',
+                                [
+                                    'idProductFk' => $idProductFk,
+                                    'idDiviceOpeningFk' => $valor['idDiviceOpeningFk']
+                                ]
                             );
                         }
                     }
@@ -55,33 +64,38 @@ class Product_model extends CI_Model {
 
     }
 
-    public function update($product) {
+    public function update($product)
+    {
 
         $this->db->set(
             [
-                'descriptionProduct'        => $product['descriptionProduct'],
-                'codigoFabric'              => $product['codigoFabric'],
-                'brand'                     => $product['brand'],
-                'model'                     => $product['model'],
+                'descriptionProduct' => $product['descriptionProduct'],
+                'codigoFabric' => $product['codigoFabric'],
+                'brand' => $product['brand'],
+                'model' => $product['model'],
                 'idProductClassificationFk' => $product['idProductClassificationFk'],
-                'isNumberSerieFabric'       => $product['isNumberSerieFabric'],
-                'isNumberSerieInternal'     => $product['isNumberSerieInternal'],
-                'isDateExpiration'          => $product['isDateExpiration'],
-                'isControlSchedule'         => $product['isControlSchedule'],
-                'isRequestNumber'           => $product['isRequestNumber'],
-                'priceFabric'               => $product['priceFabric'],
+                'isNumberSerieFabric' => $product['isNumberSerieFabric'],
+                'isNumberSerieInternal' => $product['isNumberSerieInternal'],
+                'isDateExpiration' => $product['isDateExpiration'],
+                'isControlSchedule' => $product['isControlSchedule'],
+                'isRequestNumber' => $product['isRequestNumber'],
+                'isLicenseDevice' => $product['isLicenseDevice'],
+                'priceFabric' => $product['priceFabric'],
             ]
         )->where("idProduct", $product['idProduct'])->update("tb_products");
 
         if ($product['idProductClassificationFk'] == 3) {
             if (count(@$product['list_id_divice']) > 0) {
-                $this->db->delete('tb_products_divice_opening', [ 'idProductFk' => $product['idProduct'] ]);
+                $this->db->delete('tb_products_divice_opening', ['idProductFk' => $product['idProduct']]);
 
                 foreach ($product['list_id_divice'] as $valor) {
 
-                    $this->db->insert('tb_products_divice_opening', [
-                            'idProductFk'       => $product['idProduct'],
-                            'idDiviceOpeningFk' => $valor['idDiviceOpeningFk'] ]
+                    $this->db->insert(
+                        'tb_products_divice_opening',
+                        [
+                            'idProductFk' => $product['idProduct'],
+                            'idDiviceOpeningFk' => $valor['idDiviceOpeningFk']
+                        ]
                     );
                 }
 
@@ -93,21 +107,24 @@ class Product_model extends CI_Model {
 
     }
 
-    public function delete($idProduct) {
+    public function delete($idProduct)
+    {
 
         $this->db->set(
-            [ 'idStatusFk' => -1 ])->where("idProduct", $idProduct)->update("tb_products");
+            ['idStatusFk' => -1]
+        )->where("idProduct", $idProduct)->update("tb_products");
 
         return true;
 
 
     }
 
-    public function get($id = null, $searchFilter = null) {
+    public function get($id = null, $searchFilter = null)
+    {
         $quuery = null;
-        $rs     = null;
+        $rs = null;
 
-        if (! is_null($id)) {
+        if (!is_null($id)) {
 
             $this->db->select("*")->from("tb_products");
             $this->db->join('tb_products_classification', 'tb_products_classification.idProductClassification = tb_products.idProductClassificationFk', 'inner');
@@ -161,7 +178,7 @@ class Product_model extends CI_Model {
                     $this->db->join('tb_divice_opening', 'tb_divice_opening.idDiviceOpening = tb_products_divice_opening.idDiviceOpeningFk', 'inner');
                     $quuery = $this->db->where("tb_products_divice_opening.idProductFk =", $row->idProduct)->get();
 
-                    $rs2                     = $quuery->result_array();
+                    $rs2 = $quuery->result_array();
                     $rs[$i]['diviceOpening'] = $rs2;
                     $i++;
                 }
@@ -174,7 +191,8 @@ class Product_model extends CI_Model {
         }
     }
 
-    public function getProducts4Service() {
+    public function getProducts4Service()
+    {
         $this->db->select("*")->from("tb_products_classification");
         $queryTypeProduct = $this->db->order_by("tb_products_classification.idProductClassification", "ASC")->get();
         $i = 0;
@@ -187,13 +205,13 @@ class Product_model extends CI_Model {
 
                 $this->db->select("*")->from("tb_products");
                 $this->db->join('tb_products_classification', 'tb_products_classification.idProductClassification = tb_products.idProductClassificationFk', 'inner');
-                $where_and="tb_products.idProductClassificationFk =".$rowType->idProductClassification." AND tb_products.idStatusFk !=-1";
-                $rs[$i]['idProductType']    = $producTypeId;
-                $rs[$i]['productTypeName']  = $producTypeName;
+                $where_and = "tb_products.idProductClassificationFk =" . $rowType->idProductClassification . " AND tb_products.idStatusFk !=-1";
+                $rs[$i]['idProductType'] = $producTypeId;
+                $rs[$i]['productTypeName'] = $producTypeName;
                 $queryProducts = $this->db->where($where_and)->get();
-                $rs2                     = $queryProducts->result_array();
+                $rs2 = $queryProducts->result_array();
                 $rs[$i]['products'] = $rs2;
-                $i++;    
+                $i++;
             }
             return $rs;
         }
@@ -202,4 +220,3 @@ class Product_model extends CI_Model {
 }
 
 
-	 
