080effce3fa0f3753f828a9181952f147df51da8
David <rexx84@gmail.com>
2026-04-23 17:19:21 -0300

    Update 23042026

--- sistema/Back/application/controllers/Ticket.php
+++ sistema/Back/application/controllers/Ticket.php
@@ -79,7 +79,22 @@ class Ticket extends REST_Controller
     /*Dar de baja*/
     public function index3_post()
     {
-
+        $headers = $this->input->request_headers();
+        log_message('info', '============================ Inicio Pedido de Baja ============================');
+        log_message('info', 'Host               :' . @$headers['Host']);
+        log_message('info', 'User-Agent         :' . @$headers['User-Agent']);
+        log_message('info', 'Accept             :' . @$headers['Accept']);
+        log_message('info', 'Content-Typ        :' . @$headers['Content-Type']);
+        log_message('info', 'X-Forwarded-For    :' . @$headers['X-Forwarded-For']);
+        log_message('info', 'X-Forwarded-Host   :' . @$headers['X-Forwarded-Host']);
+        log_message('info', 'X-Forwarded-Server :' . @$headers['X-Forwarded-Server']);
+        log_message('info', 'Content-Length     :' . @$headers['Content-Length']);
+        log_message('info', 'Connection         :' . @$headers['Connection']);
+        //log_message('info', 'x-signature        :' . @$headers['x-signature']);
+        //log_message('info', 'x-request-id       :' . @$headers['x-request-id']);
+        $body = file_get_contents('php://input');
+        log_message('info', 'Cuerpo del pedido de Baja: ' . $body);
+        log_message('info', '============================ Cierre Pedido de Baja ============================');
         $rs = $this->ticket_model->add3($this->post('ticket'));
         if (!is_null($rs)) {
             $this->response(array('response' => $rs), 200);
