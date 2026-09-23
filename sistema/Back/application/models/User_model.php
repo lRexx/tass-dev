364daa4fad4a10a9a8ca4f4d1d430f14939601a9
David Rincon <rexx84@gmail.com>
2026-09-23 00:13:02 -0300

    Update 22082026

--- sistema/Back/application/models/User_model.php
+++ sistema/Back/application/models/User_model.php
@@ -1098,7 +1098,7 @@ class User_model extends CI_Model
 							foreach ($deptos as $key2 => $item) {
 								log_message('debug', 'Processing depto: ' . print_r($item, true));
 								$query3 = $this->Client_model->getadmin($item['idClientFk'], null, null, null, null, null, null, null, null, null);
-								print_r($query3, true);
+								print_r($query3);
 								#log_message('debug', 'SQL: ' . $this->db->last_query());
 								#log_message('debug', 'Num rows: ' . $query3->num_rows());
 								#if ($query3->num_rows() > 0) {
